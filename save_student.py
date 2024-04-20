880bf69c75b0180be90d1aecaaa11dd22dc29f2b
2023mt03035 <161823640+2023mt03035@users.noreply.github.com>
2024-04-20 11:50:40 +0530

    Add files via upload
    devops assignment 2 project

---
+++ save_student.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('StudentDB')
+
+def lambda_handler(event, context):
+    body = json.loads(event['body'])
+    student_id = body['student_id']
+    name = body['name']
+    # You can add more attributes as needed
+
+    response = table.put_item(
+       Item={
+            'student_id': student_id,
+            'name': name
+            # Add more attributes here
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Student saved successfully!')
+    }
